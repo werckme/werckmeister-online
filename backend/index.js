7b8f238a8527f046a6610245e7de554b50b09360
samba <samba@manjaro>
2019-12-16 23:09:38 +0100

    compiler testcall impl

--- backend/index.js
+++ backend/index.js
@@ -1,60 +1,47 @@
 const { spawn } = require('child_process');
 const path = require('path')
 const WerckmeisterBinaries = "/home/samba/workspace/werckmeister/build";
-const WMCompilerBin =  path.join(WerckmeisterBinaries, 'sheetc');
+const WMCompilerBin = path.join(WerckmeisterBinaries, 'sheetc');
 
 
-const testsheet = 
-`
-using "./chords/default.chords";
-using "lua/mods/myArpeggio.lua";
-using "lua/voicings/voicelead.lua";
-
+const testsheet =
+    `
+using "./com/myChords.chords";
 device: SC1 midi 2;
 
 tempo: 130;
 instrumentDef: piano   SC1 2 0 0;
-instrumentConf: piano
-    voicingStrategy voicelead
-;
 
 [
-type: template;
-name: x;
-instrument: piano; 
+instrument: piano;
 {
-    /doOnce: myArpeggio style legato direction up/  
-    \fff
-    <I II III V VI VII>8~<I II III V VI VII>2. ~<I II III V VI VII>8 |
-    \p
-    /doOnce: myArpeggio style legato direction up/  
-    <I II III IV V VI VII>4~ <I II III IV V VI VII>2 r4  |
-}   
+    c d e f | g a b c' |
+}
 ]
 
-[
-type: template;
-name: y;
-instrument: piano; 
-{ 
-    \p
-    <I II III IV V VI VII>1
-}   
-]
-
-[
-type: accomp;
-{
-    /template: x/
-    C-7 | E-7 | C-7 | E-7 | C-7 | Gmaj7 | Bmaj7 | 
-    /template: y/
-    Cb7 | 
-}   
-]
 `
 
+const testchords =
+    `
+------------------------------------------------------------------
+X: 			I=0	    		III=4 			V=7
+Xmin: 		I=0     		III=3 			V=7
+X-: 		I=0     		III=3 			V=7
+X/7: 		I=0     		III=4 			V=7 			VII=-2
+X-/7: 		I=0     		III=3 			V=7 			VII=-2
+X/maj7: 	I=0          	III=4 			V=7 			VII=-1
+X/5: 		I=0	    		III=4 			V=-5
+X-/5: 		I=0	    		III=3 			V=-5
+X/4: 		I=0	    		III=4 	IV=-7	V=7
+X-/4: 		I=0	    		III=3 	IV=-7	V=7
+X/3: 		I=0	    		III=-8 	    	V=7
+X-/3: 		I=0	    		III=-9 	    	V=7
+X/2: 		I=0	    II=-10	III=4 	    	V=7
+`
+
+
 function executeCompiler(jsonData) {
-    return new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject) => {
         let result = "";
         const pobj = spawn(WMCompilerBin, [JSON.stringify(jsonData), '--mode=json']);
 
@@ -78,16 +65,18 @@ function executeCompiler(jsonData) {
 
 async function main() {
     try {
-    const result = await executeCompiler([{
-        path: "myfile",
-        data: testsheet
-    },
-    {
-        path: "myfile2",
-        data: "xyz"
-    }
-]);
-    console.log(result);
+        const result = await executeCompiler([
+            {
+                path: "myfile.sheet",
+                data: testsheet
+            }
+            ,
+            {
+                path: "./com/myChords.chords",
+                data: testchords
+            }
+        ]);
+        console.log(result);
     } catch (ex) {
         console.log(ex);
     }
