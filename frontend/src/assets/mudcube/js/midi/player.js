fdc2cb9368083e9332e756d466babdb3748c5f91
Johannes Unger <unger@digitalklang.de>
2020-02-16 17:52:41 +0100

    playback bug fixed

--- frontend/src/assets/mudcube/js/midi/player.js
+++ frontend/src/assets/mudcube/js/midi/player.js
@@ -290,8 +290,10 @@ var startAudio = function(currentTime, fromCache, onsuccess) {
 	for (var n = 0; n < length && messages < 100; n++) {
 		var obj = data[n];
 		if ((queuedTime += obj[1]) <= currentTime) {
-			offset = queuedTime;
-			continue;
+			if (queueTime !== currentTime) {
+				offset = queuedTime;
+				continue;
+			}
 		}
 		///
 		currentTime = queuedTime - offset;
