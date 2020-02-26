086682ebe8cc271be92db3c69be5a128117a39e2
samba <samba@manjaro>
2020-02-26 23:14:10 +0100

    style bugs fixed/max upload set to 100kb

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const config = require('config');
+var bodyParser = require('body-parser')
 
 const WerckmeisterBinaries = config.werckmeister.bin;
 const WMCompilerBin = path.join(WerckmeisterBinaries, 'sheetc');
@@ -24,7 +25,8 @@ function handle(res, func) {
     });
 }
 app.use(cors());
-app.use(express.json())
+app.use(express.json({limit: '100kb'}))
+
 
 function tryParseJSON(str) {
     try {
