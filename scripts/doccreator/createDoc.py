a4df56d60b00c00d86c5d7e3ca93c149a43eb876
samba <samba@manjaro>
2020-06-22 23:53:45 +0200

    doccreator added

---
+++ scripts/doccreator/createDoc.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+import markdown_generator as mg
+from io import StringIO
+
+heading_level = 3
+
+class ArgumentDto:
+    def __init__(self, arg_name, comments):
+        self.name = arg_name
+        self.comments = comments
+
+class CommandDto:
+    def __init__(self, command_name):
+        self.command_name = command_name
+        self.args = []
+        self.comments = None
+
+    def add_argument(self, name, comments):
+        arg = ArgumentDto(name, comments)
+        self.args.append(arg)
+
+class Printer:
+    def __init__(self, dto):
+        self.dto = dto
+
+    def comments_to_str(self, comments:str):
+        return comments.replace('///', '')
+
+    def print(self):
+        with StringIO() as stream:
+            writer = mg.Writer(stream)
+            writer.write_heading(self.dto.command_name, heading_level)
+            table = mg.Table()
+            table.add_column('parameter name')
+            table.add_column('description')
+            for arg in self.dto.args:
+                table.append(arg.name, self.comments_to_str(arg.comments))
+            writer.write(table)
+            writer.writeline()
+            writer.writeline()
+            #finito
+            stream.seek(0)
+            return stream.read()
+    
+    def __str__(self):
+        return self.print()
+    
+    def __repr__(self):
+        return str(self)
+
+def processArgumentNames(file_str):
+    import CppHeaderParser
+    main_class = "ArgumentNames"
+    cppHeader = CppHeaderParser.CppHeader(file_str)
+    names = cppHeader.classes[main_class]
+    command_dtos = []
+    for command in names["properties"]["public"]:
+        cmd_dto  = CommandDto(command['name'])
+        command_class = cppHeader.classes[f"{main_class}::C{cmd_dto.command_name}"]
+        command_dtos.append(cmd_dto)
+        for argument in command_class["properties"]["public"]:
+            comments = ""
+            if 'doxygen' in argument:
+                comments = argument['doxygen']
+            cmd_dto.add_argument(argument['name'], comments)
+    return command_dtos
+    
+
+if __name__ == '__main__':
+    import argparse
+    #parser = argparse.ArgumentParser(description='generates markdown from argumentNames.h')
+    #parser.add_argument('input', help='the input file')
+    #args = parser.parse_args()
+    #processArgumentNames(args.input)
+    dtos = processArgumentNames('/home/samba/workspace/werckmeister/src/compiler/argumentNames.h')
+    for dto in dtos:
+        print(Printer(dto))
---
+++ scripts/doccreator/test.md
@@ -0,0 +1,143 @@
+### Device
+| parameter name | description |
+|:--- |:--- |
+| WithName |  |
+| IsType |  |
+| UsePort |  |
+| OffsetIndicator |  @positional |
+| OffsetValue |  @positional |
+| WithOffset |  @named @alternative |
+
+
+
+
+### InstrumentDef
+| parameter name | description |
+|:--- |:--- |
+| WithName |  |
+| OnDevice |  |
+| Ch |  |
+| Cc |  |
+| Pc |  |
+
+
+
+
+### InstrumentConf
+| parameter name | description |
+|:--- |:--- |
+| ForInstrument |  |
+
+
+
+
+### SetVolume
+| parameter name | description |
+|:--- |:--- |
+| SetTo |  |
+
+
+
+
+### SetPan
+| parameter name | description |
+|:--- |:--- |
+| SetTo |  |
+
+
+
+
+### SetVoicingStrategy
+| parameter name | description |
+|:--- |:--- |
+| Use |  |
+
+
+
+
+### SimpleGuitarVoicingStrategy
+| parameter name | description |
+|:--- |:--- |
+| Range |  |
+
+
+
+
+### Bend
+| parameter name | description |
+|:--- |:--- |
+| Mode |  @positional |
+| Value |  @positional |
+| BendTo |  @named @alternative |
+| BendFrom |  @named @alternative |
+
+
+
+
+### AddMod
+| parameter name | description |
+|:--- |:--- |
+| Use |  |
+
+
+
+
+### SelectMidiSound
+| parameter name | description |
+|:--- |:--- |
+| CC |  |
+| PC |  |
+
+
+
+
+### SetExpression
+| parameter name | description |
+|:--- |:--- |
+| Value |  |
+
+
+
+
+### SetTempo
+| parameter name | description |
+|:--- |:--- |
+| Bpm |  |
+
+
+
+
+### SetSignature
+| parameter name | description |
+|:--- |:--- |
+| Upper |  |
+| Lower |  |
+
+
+
+
+### SetInstrument
+| parameter name | description |
+|:--- |:--- |
+| Use |  |
+
+
+
+
+### VelocityRemap
+| parameter name | description |
+|:--- |:--- |
+| PPPPP |  |
+| PPPP |  |
+| PPP |  |
+| PP |  |
+| P |  |
+| F |  |
+| FF |  |
+| FFF |  |
+| FFFF |  |
+| FFFFF |  |
+
+
+
+
