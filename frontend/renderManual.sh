f9cc8e4f929d59f241b0ffcea8f47107a5024eab
samba <samba@manjaro>
2020-07-19 14:14:06 +0200

    renderManual watchmode added

--- frontend/renderManual.sh
+++ frontend/renderManual.sh
@@ -1,2 +1,25 @@
 #!/usr/bin/sh
-node renderMarkdown.js ~/workspace/werckmeister/manual.md src/app/components/pages
+watchmode=0
+while test $# -gt 0; do
+  case "$1" in
+    --watch)
+      shift
+      
+      export watchmode=1
+      ;;
+    *)
+      break
+      ;;      
+  esac
+done
+
+render()
+{
+    node renderMarkdown.js ~/workspace/werckmeister/manual.md src/app/components/pages
+}
+
+while [ $watchmode -eq 1 ]
+do
+    inotifywait -m -q -e modify ~/workspace/werckmeister/manual.md | date +"%D %T" && render
+done
+render
