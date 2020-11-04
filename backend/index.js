5857ada00f86423b63cc6d243d4d9b34f261d669
samba <samba@manjaro>
2020-11-04 22:16:18 +0100

    new backend added

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const monk = require('monk')
+const {nanoid} = require('nanoid');
+
+require('dotenv').config();
+
+const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 1337;
+
+function createNewWorkspace() {
+    return {
+        wid: nanoid(12),
+        files: [] 
+    };
+}
+
+app.get('/', (req, res) => {
+    const workspace = createNewWorkspace();
+    res.json(workspace);
+});
+
+app.listen(port, () => {
+    console.log(`listening at port ${port}`);
+});
