05935bf24edff26f1ba6f086bb230d1b494619a7
Samba Godschynski <sambag@thinkpad.de>
2020-11-15 16:59:27 +0100

    backend: reading default workspace from directory

--- backend/emptyWorkspace.js
+++ backend/emptyWorkspace.js
@@ -1,67 +1,23 @@
-const EmptyWorkspace = {
-    files: [{
-        path: "main.sheet",
-        data: `using "default.chords";
-using "simpleComping.template";
-tempo: 140;
-device: MyDevice  midi _usePort=0;
-instrumentDef:lead    _onDevice=MyDevice  _ch=0 _pc=0;
-instrumentDef:rhtythm _onDevice=MyDevice  _ch=1 _pc=0;
-instrumentDef:bass    _onDevice=MyDevice  _ch=2 _pc=0;
+const fs = require("fs");
+const workspaceDir = './defaultWorkspace';
+let EmptyWorkspace = null;
 
--- melody track
-[
-instrument: lead;
-{
-    \\fff
-    r4 e f# g  | c'1~       | c'4 d e f#  | b2 b2~ | 
-    b4 c d e   | a1~        | a4 b, c# d# | g1     |
-    r4 e f# g  | c'1~       | c'4 d e f#  | b2 b2~ | 
-    b4 c d e   | a1~        | a4 f# a g   | e1~    |
-    e4 r d# e  | f# b, f#2~ | f#4 f# e f# | g1~    |
-    g4 g f# g  | a1~        | a4 d d' c'  | b1~    | 
-    b4 r a# b  | c' c' a a  | f#2. c'4    | b2 b2~ | 
-    b2. e4     | a2. g4     | f#2 g4 b,   | 
-    e1 
-}
-]
 
--- accomp track
-[
-type: accomp;
-{   
-    r      | A-7    | D7    | Gmaj7  | 
-    Cmaj7  | F#-7b5 | B7    | E-     | 
-    E-     | A-7    | D7    | Gmaj7  | 
-    Cmaj7  | F#-7b5 | B7    | E-     | 
-    E-     | F#-7b5 | B7b9  | E-     | 
-    E-     | A-7    | D7    | Gmaj7  |
-    Gmaj7  | F#-7b5 | B7b9  | E-7 A7 | 
-    D-7 G7 | F#-7b5 | B7b9  | E-     |
+function loadWorkspaceFiles() {
+    const dir = fs.readdirSync(workspaceDir);
+    return {
+        files: dir.map(file => ({
+            path: file,
+            data: fs.readFileSync(`${workspaceDir}/${file}`).toString()
+        }))
+    };
 }
-]`},{
-    path: "simpleComping.template",
-    data: `-- the templates defines what the accomp track has to play
-[
-type: template;
-name: myAccomp;
-instrument: rhtythm;
-{
-    \\p
-    <III, V, VII, II>1 | 
-}
-]
 
-[
-type: template;
-name: myAccomp;
-instrument: bass;
-{
-    \\p
-    I,,2. V,,4 |
+function GetEmptyWorkspace() {
+    if (!EmptyWorkspace) {
+        EmptyWorkspace = loadWorkspaceFiles();
+    }
+    return EmptyWorkspace;
 }
-]`    
-    }]
-};
 
-module.exports = EmptyWorkspace;
+module.exports = GetEmptyWorkspace;
--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const monk = require('monk')
 const {nanoid} = require('nanoid');
-const EmptyWorkspace = require('./emptyWorkspace');
+const GetEmptyWorkspace = require('./emptyWorkspace');
 const slowDown = require("express-slow-down");
 const yup = require('yup');
 var bodyParser = require('body-parser')
@@ -41,7 +41,7 @@ const port = process.env.PORT || 1337;
 function createNewWorkspace() {
     return {
         wid: nanoid(12),
-        files: EmptyWorkspace.files 
+        files: GetEmptyWorkspace().files 
     };
 }
 
