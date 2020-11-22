226facc9f7a43bae74de53c2e3ec373a79edc858
Samba Godschynski <sambag@thinkpad.de>
2020-11-22 23:34:09 +0100

    missing files added

---
+++ backend/deploy_backend.sh
@@ -0,0 +1,3 @@
+zip wmonline-backend.zip -r config index.js localWorkspaceBuilder.js package.json presets
+scp wmonline-backend.zip sambag@sambag.uber.space:
+ssh sambag@sambag.uber.space "sh deploy_werckmeister_backend.sh"
