a77c76462f4b2e90518b9ad5b020634917ed49be
Samba Godschynski <sambag@thinkpad.de>
2020-11-23 00:10:58 +0100

    environment config updated

--- frontend/releaseFrontendFiles.sh
+++ frontend/releaseFrontendFiles.sh
@@ -18,4 +18,5 @@ mkdir -p getting-started && cp index.html getting-started/
 mkdir -p examples && cp index.html examples/
 mkdir -p code-extension && cp index.html code-extension/
 mkdir -p getwerckmeister && cp index.html getwerckmeister/
+mkdir -p editor && cp index.html editor/
 cd $cdir
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   defaultMidiport: 1,
-  workspaceStorage: null,
+  workspaceStorage: 'https://sambag.uber.space/api-wm',
   githubApi: 'https://api.github.com',
   soundfontUrl: 'https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/', //'./assets/soundfont/',
 };
