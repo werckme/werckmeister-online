53e6e5a4ba94f32efdc5a62c91c7f37dea322f19
samba <samba@manjaro>
2021-02-09 23:01:36 +0100

    home page reworded, redesigned

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public gutter = {xs:0, md: 32, lg: 64, xl: 100, xxl: 100};
+
   constructor() { }
 
   ngOnInit() {
