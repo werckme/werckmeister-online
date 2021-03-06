354c154d015c3d7ffefd064ab0c33faae5fc0128
samba <samba@manjaro>
2021-03-07 00:39:48 +0100

    fluid synth midi player used

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
   defaultMidiport: 1,
-  workspaceStorage: 'https://sambag.uber.space/api-wm',
-  githubApi: 'https://api.github.com',
-  soundfontUrl: 'https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/', //'./assets/soundfont/',
+  workspaceStorage: 'https://sambag.uber.space/api-wm'
 };
