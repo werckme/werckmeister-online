c596c66a3534535588268467964e7d5096106ad8
samba <samba@manjaro>
2022-06-18 17:41:11 +0200

    polish

--- backend/updateIndices.js
+++ backend/updateIndices.js
@@ -4,24 +4,24 @@ const { getSheetMetaData, getExternalResourceMetaData, getStyleTemplateMetaData 
 
 require('dotenv').config();
 
-function updatePreset(preset, docs) {
+async function updatePreset(preset, docs) {
     console.log(`UPDATE ${preset.wid}`)
-    return docs.update({wid: preset.wid}, {$set: preset}, {upsert: true});
+    await docs.update({wid: preset.wid}, {$set: preset}, {upsert: true});
 }
 
-function updateExternalResource(externalResource, docs) {
+async function updateExternalResource(externalResource, docs) {
     console.log(`UPDATE ${externalResource.eid}`)
-    return docs.update({eid: externalResource.eid}, {$set: externalResource}, {upsert: true});
+    await docs.update({eid: externalResource.eid}, {$set: externalResource}, {upsert: true});
 }
 
-function updateTemplate(template, docs) {
+async function updateTemplate(template, docs) {
     const id = template.metaData.id;
     console.log(`UPDATE ${id}`)
-    return docs.update({id: id}, {$set: template}, {upsert: true});
+    await docs.update({id: id}, {$set: template}, {upsert: true});
 }
 
 
-async function main() {
+(async function main() {
     const db = monk(process.env.MONGODB_URI);
     const dbpresets = db.get('presets');
     await dbpresets.createIndex({ wid: 1 }, { unique: true });
@@ -32,28 +32,25 @@ async function main() {
     const dbStyleTemplates = db.get('styleTemplates');
     await dbStyleTemplates.createIndex({ id: 1 }, { unique: true });
 
+    let promises = listPresets() 
+        .map(x => ({metaData: getSheetMetaData(`./presets/${x}/main.sheet`), wid: x}))
+        .map(x => updatePreset(x, dbpresets));
+
+    promises = promises.concat(listExternalResources()
+        .map(x => ({metaData: getExternalResourceMetaData(`./externalResources/${x}`), eid: x}))
+        .map(x => updateExternalResource(x, dbexternal))
+    );
+
+    promises = promises.concat(listTemplates()
+        .map(x => ({metaData: getStyleTemplateMetaData(`./style-templates/${x}`)}))
+        .map(x => updateTemplate(x, dbStyleTemplates))
+    );
 
     try {
-        let promises = listPresets() 
-            .map(x => ({metaData: getSheetMetaData(`./presets/${x}/main.sheet`), wid: x}))
-            .map(x => updatePreset(x, dbpresets));
-        await Promise.all(promises);
-
-        promises = listExternalResources()
-            .map(x => ({metaData: getExternalResourceMetaData(`./externalResources/${x}`), eid: x}))
-            .map(x => updateExternalResource(x, dbexternal));
-        await Promise.all(promises);
-
-        promises = listTemplates()
-            .map(x => ({metaData: getStyleTemplateMetaData(`./style-templates/${x}`)}))
-            .map(x => updateTemplate(x, dbStyleTemplates));
-        await Promise.all(promises);
-        
+        x = await Promise.all(promises);
     } catch(ex) {
     } finally {
         await db.close();
     }
-}
-
-main();
+})();
 
